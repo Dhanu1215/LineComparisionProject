9ee060603fb6f80b7d84a6e89a2b22180d084641
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-10-19 09:57:16 +0530

    Added: Length calculation file is added

--- src/com/blp/linecomparisionproject/LineComparisonComputation.java
+++ src/com/blp/linecomparisionproject/LineComparisonComputation.java
@@ -7,7 +7,13 @@ package com.blp.linecomparisionproject;
 public class LineComparisonComputation {
 
     public static void main(String[] args) {
-
-        System.out.println("Welcome To Line Comparison Computation Program");
+        //Declaring and Initializing the variables
+        double x1 = 2;
+        double x2 = 4;
+        double y1 = 6;
+        double y2 = 8;
+        //Use Math function to calculate length of line
+        double lengthOfLine = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        System.out.println("Length Of Line = " + lengthOfLine);
     }
 }
