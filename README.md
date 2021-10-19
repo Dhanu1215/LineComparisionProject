e505df7ac25a13ab2226bc54e36aa0b9c58cc5c5
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-10-19 08:31:39 +0530

    Added: Welcome To Line Comparison Computation Program message is added

---
+++ README.md
@@ -0,0 +1,1 @@
+LineComparisionProjectDay3
---
+++ src/com/blp/linecomparisionproject/LineComparisonComputation.java
@@ -0,0 +1,13 @@
+package com.blp.linecomparisionproject;
+
+/**
+ * Line Comparison Computation Program
+ */
+
+public class LineComparisonComputation {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To Line Comparison Computation Program");
+    }
+}
